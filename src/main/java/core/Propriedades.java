be9e63ab7140be6bbd586317524627170a946b9b
Renan <renans.pereira88@gmail.com>
2018-06-26 21:19:38 -0300

    alteração do navegador

--- src/main/java/core/Propriedades.java
+++ src/main/java/core/Propriedades.java
@@ -4,7 +4,7 @@ public class Propriedades {
 	
 	public static boolean FECHAR_BROWSER = false;
 	
-	public static Browsers browser = Browsers.CHROME;
+	public static Browsers browser = Browsers.FIREFOX;
 	
 	public static String NOME_CONTA_ALTERADA = "Conta Alterada " + System.nanoTime();  
 	
